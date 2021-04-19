a54102b4c809ceb9a04bf69b70f9dc4f8ab13f39
manojbhatta <jmsbhatta@gmail.com>
2021-04-19 10:01:10 +0545

    cleared sidebar

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -82,7 +82,7 @@ class RouteServiceProvider extends ServiceProvider
 
     protected function mapBackendRoutes()
     {
-        Route::middleware('web')
+        Route::middleware(['web', 'auth'])
             ->namespace($this->backendNamespace)
             ->prefix('backend')
             ->group(base_path('routes/backend.php'));
