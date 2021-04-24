6ea2763c529fe5c4a1ac3e7f3a03cf258f1b4a2e
manojbhatta <bob@example.com>
2021-04-24 22:33:26 +0545

    ui

--- resources/views/frontend/product/index.blade.php
+++ resources/views/frontend/product/index.blade.php
@@ -3,12 +3,21 @@
         Products | {{ config('app.name') }}
     </x-slot>
 
-    <div class="container py-3">
+    <div class="container-fluid bg-light">
+        <div class="container py-4">
+            <h1 class="text-dark-gray">Products</h1>
+            <div>
+                Home / Mixer & Grinder /Products
+            </div>
+        </div>
+    </div>
+
+    <div class="container py-4">
         <div class="row">
             <div class="col-md-3">
-                <h4 class="text-dark-gray">Filters</h4>
+                {{-- <h4 class="text-dark-gray">Filters</h4> --}}
 
-               <x-frontend.partials.category-filter-list></x-frontend.partials.category-filter-list>
+                <x-frontend.partials.category-filter-list></x-frontend.partials.category-filter-list>
                 <div class="my-4"></div>
 
                 <x-frontend.partials.block-title-box title="Filter By Price" />
@@ -20,13 +29,25 @@
                         </select>
                         <input type="text" class="form-control rounded-0 ml-md-4" placeholder="Rs. xxx">
                     </div>
-                    <button class="btn btn-danger btn-sm bg-theme-color rounded-0 px-3 px-md-4">Filter</button>
+                    <button class="btn btn-danger btn-sm bg-theme-accent-color rounded-0 px-3 px-md-4">Filter</button>
                 </form>
 
             </div>
             <div class="col-md-9">
+                <div class="d-flex mb-2">
+                    <div class="align-self-center">
+                        32 items found
+                    </div>
+                    <div class="ml-auto">
+                        <select name="" class="custom-select">
+                            <option value="">Recommended</option>
+                            <option value="">High to Low</option>
+                            <option value="">Low to High</option>
+                        </select>
+                    </div>
+                </div>
                 <div>
-                    <h4 class="section-text__dark__gray">Our Products</h4>
+                    {{-- <h4 class="section-text__dark__gray">Our Products</h4> --}}
                 </div>
                 <section class="">
                     <div class="row">
