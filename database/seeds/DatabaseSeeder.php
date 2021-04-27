45719224b94c9dd5969847d24560bb51f0f3d94b
manojbhatta <bob@example.com>
2021-04-27 16:07:55 +0545

    Remove product seeder from seed queue

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
-        $this->call(CategorySeeder::class);
+        // $this->call(CategorySeeder::class);
         // check if environment is local
         $this->call(ProductSeeder::class);
     }
