ab4889e7bc177611661ed050aa6a39e0c4395532
manojbhatta <bob@example.com>
2021-04-28 15:46:26 +0545

    wip

--- app/Service/ProductImageService.php
+++ app/Service/ProductImageService.php
@@ -24,7 +24,7 @@ class ProductImageService
             'featured' => $featured
         ]);
 
-        // $this->imageService->createThumbnail( Storage::path('storage/' . $productImage['thumbnail_path']), null, 350);
+        $this->imageService->createThumbnail( Storage::path($productImage['thumbnail_path']), null, 350);
         // $this->imageService->createThumbnail('storage/' . $productImage['medium_path'], null, 600);
 
         $product->images()->save($productImage);
