04c4b10af99dfeaf57ff58e61eecf9b2b355cb3b
manojbhatta <bob@example.com>
2021-05-17 14:53:29 +0545

    LWP-59

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.backend')
 
 @section('content')
-<div class="container">
+<div class="container-fluid py-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
--- resources/views/layouts/backend/sidebar.blade.php
+++ resources/views/layouts/backend/sidebar.blade.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-light-indigo elevation-1">
     <!-- Brand Logo -->
-    <a href="{{ url('/') }}" class="brand-link" target="_blank">
-        <img src="{{ asset('adminlte') }}/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-1" style="opacity: .8">
-        <span class="brand-text font-weight-light">{{ siteName() }}</span>
+    <a href="{{ url('/') }}" class="brand-link text-center" target="_blank">
+        {{-- <span class="brand-text font-weight-light">{{ siteName() }}</span> --}}
+        <img src="{{ siteLogoUrl() }}" alt="" style="max-width: 100%; height: 2.5rem;" alt="{{ siteName() }}">
     </a>
 
     <!-- Sidebar -->
@@ -29,14 +29,14 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('backend.categories.index') }}" class="nav-link">
+                    <a href="{{ route('backend.categories.index') }}" class="nav-link {{ setActive('backend.categories.*') }}">
                         <i class="nav-icon fa fa-list"></i>
                         <p>
                             Category
                         </p>
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if(request()->routeIs('backend.products.index')) menu-tree @endif">
                     <a href="{{ route('backend.products.index') }}" class="nav-link">
                         <i class="nav-icon fa fa-box"></i>
                         <p>
@@ -80,7 +80,7 @@
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('backend.contact-us.index') }}" class="nav-link">
+                    <a href="{{ route('backend.contact-us.index') }}" class="nav-link {{ setActive('backend.contact-us.*') }}">
                         <i class="nav-icon fa fa-comments"></i>
                         <p>
                             Contact Messages
@@ -89,23 +89,23 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('backend.stores.index') }}" class="nav-link">
+                    <a href="{{ route('backend.stores.index') }}" class="nav-link {{ setActive('backend.stores.*') }}">
                         <i class="nav-icon fa fa-store"></i>
                         <p>
                             Stores
                         </p>
                     </a>
                 </li>
-                <li class="nav-item">
+                {{-- <li class="nav-item">
                     <a href="{{ route('backend.contact-us.index') }}" class="nav-link">
                         <i class="nav-icon far fa-user"></i>
                         <p>
                             Users
                         </p>
                     </a>
-                </li>
+                </li> --}}
                 <li class="nav-item">
-                    <a href="{{ route('backend.subscribers.index') }}" class="nav-link">
+                    <a href="{{ route('backend.subscribers.index') }}" class="nav-link {{ setActive('backend.subscribers.*') }}">
                         <i class="nav-icon fa fa-users"></i>
                         <p>
                             Subscribers
@@ -120,8 +120,8 @@
                         </p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
+                <li class="nav-item @if(request()->routeIs('backend.settings.*')) menu-open @endif">
+                    <a href="#" class="nav-link {{ setActive('backend.settings.*') }}">
                         <i class="nav-icon fas fa-cog"></i>
                         <p>
                             Settings
@@ -130,28 +130,28 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{ route('backend.settings.general.index') }}" class="nav-link">
+                            <a href="{{ route('backend.settings.general.index') }}" class="nav-link {{ setActive('backend.settings.general.index') }}">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>General</p>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a href="{{ route('backend.settings.page.index') }}" class="nav-link">
+                        {{-- <li class="nav-item">
+                            <a href="{{ route('backend.settings.page.index') }}" class="nav-link {{ setActive('backend.settings.page.index') }}">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>API & Keys</p>
                             </a>
-                        </li>
+                        </li> --}}
                         <li class="nav-item">
-                            <a href="{{ route('backend.settings.page.index') }}" class="nav-link">
+                            <a href="{{ route('backend.settings.page.index') }}" class="nav-link {{ setActive('backend.settings.page.index') }}">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Page Settings</p>
                             </a>
                         </li>
                     </ul>
                 </li>
-                <li class="nav-header">CMS</li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
+                {{-- <li class="nav-header">CMS</li> --}}
+                <li class="nav-item @if(request()->routeIs('backend.pages.*')) menu-open @endif">
+                    <a href="#" class="nav-link {{ setActive('backend.pages.*') }}">
                         <i class="nav-icon fa fa-file"></i>
                         <p>
                             Pages
@@ -160,13 +160,13 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{ route('backend.pages.index') }}" class="nav-link">
+                            <a href="{{ route('backend.pages.index') }}" class="nav-link {{ setActive('backend.pages.index') }}">
                                 <i class="far fa-file-image nav-icon"></i>
                                 <p>List All</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ route('backend.pages.create-or-edit') }}" class="nav-link">
+                            <a href="{{ route('backend.pages.create-or-edit') }}" class="nav-link {{ setActive('backend.pages.create-or-edit') }}">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Add Page</p>
                             </a>
@@ -174,8 +174,8 @@
                     </ul>
                 </li>
 
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
+                <li class="nav-item @if(request()->routeIs('backend.image-sliders.*')) menu-open @endif">
+                    <a href="#" class="nav-link {{ setActive('backend.image-sliders.*') }}">
                         <i class="nav-icon fa fa-file-image"></i>
                         <p>
                             Image Sliders
@@ -184,13 +184,13 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{ route('backend.image-sliders.index') }}" class="nav-link">
+                            <a href="{{ route('backend.image-sliders.index') }}" class="nav-link {{ setActive('backend.image-sliders.index') }}">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>List All</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ route('backend.image-sliders.create') }}" class="nav-link">
+                            <a href="{{ route('backend.image-sliders.create') }}" class="nav-link {{ setActive('backend.image-sliders.create') }}">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Add New</p>
                             </a>
