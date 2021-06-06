8db15c2a02727709fc332687b32644f4d42f0fbd
manojbhatta <bob@example.com>
2021-06-06 14:49:46 +0545

    controller for user api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get('/user', 'API\UserController');
 
 Route::name('api.')->group(function () {
     Route::get('image-sliders', 'API\ImageSliderController@index')->name('image-sliders.index');
