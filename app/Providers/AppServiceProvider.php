5beb96b3cd57c61743c55dd167790a14f638b2a8
manojbhatta <bob@example.com>
2021-06-29 10:15:05 +0545

    check app environment from config instead of .env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,7 +30,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // Force https
-        if (env('APP_ENV') === 'production') {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
 
