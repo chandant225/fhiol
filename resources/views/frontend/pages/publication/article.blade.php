cd4a6dc90d2838b0291ee6d21a622a6e12b02796
kamal khatri <59205492+ZERO-KAMAL@users.noreply.github.com>
2021-09-22 17:11:38 +0545

    added publication page

---
+++ resources/views/frontend/pages/publication/article.blade.php
@@ -0,0 +1,260 @@
+<x-app-layout>
+
+
+
+    <!-- publicationMain -->
+    <section class="publicationMain">
+        <!-- ======= Breadcrumbs ======= -->
+        <section id="breadcrumbs" class="breadcrumbs">
+            <div class="container">
+
+                <ol>
+                    <li><a href="index.html">Home</a></li>
+                    <li>Publication</li>
+                </ol>
+                <h2>Article</h2>
+
+            </div>
+        </section><!-- End Breadcrumbs -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="publication-category">
+                        <h6 class="col-md-12 bg-light align-items-center"><span class="material-icons me-2">
+                                category
+                            </span>Publication categories</h6>
+                        <ul>
+                            <li><a href="{{route('publication.news')}}" class=" d-flex align-items-center"><span
+                                        class="material-icons me-2">
+                                        grain
+                                    </span>News & Events</a>
+                            </li>
+
+                            </li>
+                            <li><a href="{{route('publication.article')}}" class=" d-flex align-items-center"><span
+                                        class="material-icons me-2">
+                                        grain
+                                    </span>Article</a>
+
+
+
+                            </li>
+                            <li><a href="{{route('publication.download')}}" class=" d-flex align-items-center"><span
+                                        class="material-icons me-2">
+                                        grain
+                                    </span>Downloads</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="row mt-2">
+                        <div class="col-md-12 d-flex two-btn">
+                            <div class="title-left">
+                                <h4>Our Article</h4>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        <!-- Post-->
+                        <!-- Post-->
+                        <div class="col-xl-4 col-md-6 col-12 ">
+                            <div data-aos="fade-up" data-aos-duration="1500">
+                                <a href="{{route('publication.publicationdetails')}}" class=" text-decoration-none">
+                                    <div class=" post-module">
+                                        <div class="category">10 FEB. 2021</div>
+                                        <!-- Thumbnail-->
+                                        <div class="thumbnail">
+
+                                            <img src="./img/news-1.jpg" />
+                                        </div>
+                                        <!-- Post Content-->
+                                        <div class="post-content">
+
+                                            <h1 class="title">City Lights in New York</h1>
+                                            <h2 class="sub_title">The city that never sleeps.</h2>
+                                            <p class="description">New York, the largest city in the U.S., is an
+                                                architectural
+                                                marvel
+                                                with
+                                                plenty of historic monuments, magnificent buildings and countless
+                                                dazzling
+                                                skyscrapers.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+
+                        </div>
+                        <div class="col-xl-4 col-md-6 col-12 news-respon">
+                            <div data-aos="fade-up" data-aos-duration="1500">
+                                <a href="{{route('publication.publicationdetails')}}" class=" text-decoration-none">
+                                    <div class=" post-module">
+                                        <div class="category">10 FEB. 2021</div>
+                                        <!-- Thumbnail-->
+                                        <div class="thumbnail">
+
+                                            <img src="./img/news-1.jpg" />
+                                        </div>
+                                        <!-- Post Content-->
+                                        <div class="post-content">
+
+                                            <h1 class="title">City Lights in New York</h1>
+                                            <h2 class="sub_title">The city that never sleeps.</h2>
+                                            <p class="description">New York, the largest city in the U.S., is an
+                                                architectural
+                                                marvel
+                                                with
+                                                plenty of historic monuments, magnificent buildings and countless
+                                                dazzling
+                                                skyscrapers.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+
+                        </div>
+                        <div class="col-xl-4 col-md-6 col-12 news-respon">
+                            <div data-aos="fade-up" data-aos-duration="1500">
+                                <a href="{{route('publication.publicationdetails')}}" class=" text-decoration-none">
+                                    <div class=" post-module">
+                                        <div class="category">10 FEB. 2021</div>
+                                        <!-- Thumbnail-->
+                                        <div class="thumbnail">
+
+                                            <img src="./img/news-1.jpg" />
+                                        </div>
+                                        <!-- Post Content-->
+                                        <div class="post-content">
+
+                                            <h1 class="title">City Lights in New York</h1>
+                                            <h2 class="sub_title">The city that never sleeps.</h2>
+                                            <p class="description">New York, the largest city in the U.S., is an
+                                                architectural
+                                                marvel
+                                                with
+                                                plenty of historic monuments, magnificent buildings and countless
+                                                dazzling
+                                                skyscrapers.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                    <div class="row mt-4">
+                        <!-- Post-->
+                        <div class="col-xl-4 col-md-6 col-12 ">
+                            <div data-aos="fade-up" data-aos-duration="1500">
+                                <a href="{{route('publication.publicationdetails')}}" class=" text-decoration-none">
+                                    <div class=" post-module">
+                                        <div class="category">10 FEB. 2021</div>
+                                        <!-- Thumbnail-->
+                                        <div class="thumbnail">
+
+                                            <img src="./img/news-1.jpg" />
+                                        </div>
+                                        <!-- Post Content-->
+                                        <div class="post-content">
+
+                                            <h1 class="title">City Lights in New York</h1>
+                                            <h2 class="sub_title">The city that never sleeps.</h2>
+                                            <p class="description">New York, the largest city in the U.S., is an
+                                                architectural
+                                                marvel
+                                                with
+                                                plenty of historic monuments, magnificent buildings and countless
+                                                dazzling
+                                                skyscrapers.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+
+                        </div>
+                        <div class="col-xl-4 col-md-6 col-12 news-respon">
+                            <div data-aos="fade-up" data-aos-duration="1500">
+                                <a href="{{route('publication.publicationdetails')}}" class=" text-decoration-none">
+                                    <div class=" post-module">
+                                        <div class="category">10 FEB. 2021</div>
+                                        <!-- Thumbnail-->
+                                        <div class="thumbnail">
+
+                                            <img src="./img/news-1.jpg" />
+                                        </div>
+                                        <!-- Post Content-->
+                                        <div class="post-content">
+
+                                            <h1 class="title">City Lights in New York</h1>
+                                            <h2 class="sub_title">The city that never sleeps.</h2>
+                                            <p class="description">New York, the largest city in the U.S., is an
+                                                architectural
+                                                marvel
+                                                with
+                                                plenty of historic monuments, magnificent buildings and countless
+                                                dazzling
+                                                skyscrapers.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+
+                        </div>
+                        <div class="col-xl-4 col-md-6 col-12 news-respon">
+                            <div data-aos="fade-up" data-aos-duration="1500">
+                                <a href="{{route('publication.publicationdetails')}}" class=" text-decoration-none">
+                                    <div class=" post-module">
+                                        <div class="category">10 FEB. 2021</div>
+                                        <!-- Thumbnail-->
+                                        <div class="thumbnail">
+
+                                            <img src="./img/news-1.jpg" />
+                                        </div>
+                                        <!-- Post Content-->
+                                        <div class="post-content">
+
+                                            <h1 class="title">City Lights in New York</h1>
+                                            <h2 class="sub_title">The city that never sleeps.</h2>
+                                            <p class="description">New York, the largest city in the U.S., is an
+                                                architectural
+                                                marvel
+                                                with
+                                                plenty of historic monuments, magnificent buildings and countless
+                                                dazzling
+                                                skyscrapers.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- publicationMain-end -->
+
+    @push('scripts')
+    @endpush
+
+</x-app-layout>
---
+++ resources/views/frontend/pages/publication/download.blade.php
@@ -0,0 +1,78 @@
+<x-app-layout>
+
+
+
+    <main id="main">
+
+        <!-- ======= Breadcrumbs ======= -->
+        <section id="breadcrumbs" class="breadcrumbs">
+            <div class="container">
+
+                <ol>
+                    <li><a href="index.html">Home</a></li>
+                    <li>Publication</li>
+                </ol>
+                <h2>Download</h2>
+
+            </div>
+        </section><!-- End Breadcrumbs -->
+
+        <section class="download">
+            <div class="container">
+                <div class="title-left">
+                    <h4>Downloads</h4>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 d-flex justify-content-between">
+                        <h6>In publishing and graphic design,</h6>
+                        <a href=""><i class="fas fa-cloud-download-alt"></i> Download</a>
+                    </div>
+                    <hr>
+                    <div class="col-md-12 d-flex justify-content-between">
+                        <h6>In publishing and graphic design,</h6>
+                        <a href=""><i class="fas fa-cloud-download-alt"></i> Download</a>
+                    </div>
+                    <hr>
+                    <div class="col-md-12 d-flex justify-content-between">
+                        <h6>In publishing and graphic design,</h6>
+                        <a href=""><i class="fas fa-cloud-download-alt"></i> Download</a>
+                    </div>
+                    <hr>
+                    <div class="col-md-12 d-flex justify-content-between">
+                        <h6>In publishing and graphic design,</h6>
+                        <a href=""><i class="fas fa-cloud-download-alt"></i> Download</a>
+                    </div>
+                    <hr>
+                    <div class="col-md-12 d-flex justify-content-between">
+                        <h6>In publishing and graphic design,</h6>
+                        <a href=""><i class="fas fa-cloud-download-alt"></i> Download</a>
+                    </div>
+                    <hr>
+                    <div class="col-md-12 d-flex justify-content-between">
+                        <h6>In publishing and graphic design,</h6>
+                        <a href=""><i class="fas fa-cloud-download-alt"></i> Download</a>
+                    </div>
+                    <hr>
+                    <div class="col-md-12 d-flex justify-content-between">
+                        <h6>In publishing and graphic design,</h6>
+                        <a href=""><i class="fas fa-cloud-download-alt"></i> Download</a>
+                    </div>
+                    <hr>
+                    <div class="col-md-12 d-flex justify-content-between">
+                        <h6>In publishing and graphic design,</h6>
+                        <a href=""><i class="fas fa-cloud-download-alt"></i> Download</a>
+                    </div>
+                    <hr>
+
+
+                </div>
+
+            </div>
+        </section>
+
+    </main><!-- End #main -->
+
+    @push('scripts')
+    @endpush
+
+</x-app-layout>
